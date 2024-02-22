49d2b363c5b0688ebd7a9dcb50c485f215506cd2
demandsupply <giova.zol92@gmail.com>
2024-02-22 22:37:07 +0100

    obtained the tmdb api key, created a search movies form on index.html which requests the api data and displayed the raw data in index.html

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
 import os
 from flask import Flask, redirect, render_template, request
 from cs50 import SQL
+import requests
 
 # Configure application
 app = Flask(__name__)
 
 
 
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3Yjg3YzFlNzU4ZTNkNzA4YzUyMmUyYmUyN2FjYjBhMCIsInN1YiI6IjY1Y2U3MzM0YTMxNDQwMDE2MmE2ZGMwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Azo3xqnhGWGHEv7B_Genf96HVFJcEBfJki1_vZBN0W0"
+}
+
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("index.html")
+        movie = request.args.get("movie")
+        print(movie)
+
+        url = "https://api.themoviedb.org/3/search/movie?query={0}".format(movie)
+
+        print(url)
+        response = requests.get(url, headers=headers)
+
+        print(response.text)
+
+        return render_template("index.html", movie=movie, response=response.text)
+    else:
+        movie = request.form.get("movie")
+        print(movie)
+        return redirect("/")
