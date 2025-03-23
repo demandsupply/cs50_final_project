3b6b22feda78064e31dba560b8942c6501e95c6c
demandsupply <giova.zol92@gmail.com>
2025-03-23 17:13:57 +0100

    style movie carousel and its content

--- static/main.js
+++ static/main.js
@@ -96,6 +96,38 @@ if (document.getElementById("showCarouselList")) {
         list.scrollLeft += (listWidth - itemWidth);
     })
 }
+window.addEventListener("load", (event) => {
+    if (document.getElementById("imgCarouselList")) {
+        const prevbtn = document.getElementById('imgBtnPrev');
+        const nextbtn = document.getElementById("imgBtnNext");
+        const list = document.getElementById("imgCarouselList");
+        const listElements = document.getElementById("imgCarouselList").children;
+    
+        // Obtain the with of the carousel item
+        const firstItemStyle = window.getComputedStyle(listElements[0]);
+        const itemWidthStr = firstItemStyle.getPropertyValue("width");
+        const itemWidth = Number(itemWidthStr.substring(0, itemWidthStr.length-2))
+        let listWidth = list.offsetWidth;
+    
+        // On smaller screens the buttons will scroll less
+        if (window.innerWidth < 641) {
+            listWidth = itemWidth * 2;
+        }
+    
+        // Clicknig on buttons will scroll the carousel
+        prevbtn.addEventListener("click", ()=> {
+            list.scrollLeft -= (listWidth - itemWidth);
+        })
+        nextbtn.addEventListener("click", ()=> {
+            list.scrollLeft += (listWidth - itemWidth);
+        })
+
+        list.scrollLeft = 0;
+
+    }
+});
+
+
 
 
 // Refreshing the page reset the carousel index
