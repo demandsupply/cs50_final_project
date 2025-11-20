5575bc241eb81f1c6deaec75c0edf90f86c3c372
demandsupply <giova.zol92@gmail.com>
2025-11-20 22:23:33 +0100

    Add: implement favorite management functions for user items

---
+++ helpers/dbQueries.py
@@ -0,0 +1,31 @@
+from cs50 import SQL
+
+db = SQL("sqlite:///finalproject.db")
+
+def is_favorite(user, item_id, type):
+    rows = db.execute(
+        "SELECT * FROM favoriteswatchlist" \
+        "WHERE username=? AND" \
+        "item_id=? AND" \
+        "type=? AND" \
+        "category = 'favorite'", 
+        user, item_id, type
+    )
+    return len(rows) == 1
+
+
+def add_favorite(user, item_id, type, title):
+    db.execute(
+        "INSERT INTO favoriteswatchlist (username, type, title, item_id, category)" \
+        "VALUES(?, ?, ?, ?, 'favorite')",
+        user, type, title, item_id
+    )
+
+
+def remove_favorite(user, item_id):
+    db.execute(
+        "DELETE FROM favoriteswatchlist WHERE username=? AND" \
+        "item_id=? AND" \
+        "category='favorite",
+        user, item_id
+    )
