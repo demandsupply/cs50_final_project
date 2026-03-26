f013d681367e77d55226561ac1e31a14ce3439db
demandsupply <giova.zol92@gmail.com>
2026-03-26 15:27:36 +0100

    Fix: fixed issue where only logged in users can see a movie page; now movie pages can be seen by guests - in this case buttons add favorite/watchlist are hidden

--- routes/movies.py
+++ routes/movies.py
@@ -9,7 +9,8 @@ movies_bp = Blueprint("movies", __name__)
 
 @movies_bp.route("/movie/<id>", methods = ["GET", "POST"])
 def movie(id):
-    username = get_username(session["user_id"])
+    user_id = session.get("user_id")
+    username = user_id if user_id else "guest"
     movie_datas = tmdb_get(f"movie/{id}")
     if not movie_datas:
         return "Movie not found", 404
@@ -21,28 +22,34 @@ def movie(id):
 
     if request.method == ("GET"):
         print("request method is get")
-        
-        button_favorites = "remove from favorites" if is_favorite(username, id, "movie") else "add to favorites"
-        button_watchlist = "remove from watchlist" if is_in_watchlist(username, id, "movie") else "add to watchlist"
 
-        return render_template ("movie.html", imgMovie_datas = imgMovie_datas,  movie_datas=movie_datas, button_favorites=button_favorites, button_watchlist=button_watchlist)
+        if user_id:
 
+            button_favorites = "remove from favorites" if is_favorite(username, id, "movie") else "add to favorites"
+            button_watchlist = "remove from watchlist" if is_in_watchlist(username, id, "movie") else "add to watchlist"
+
+            return render_template ("movie.html", imgMovie_datas = imgMovie_datas,  movie_datas=movie_datas, button_favorites=button_favorites, button_watchlist=button_watchlist)
+        else:
+            return render_template ("movie.html", imgMovie_datas = imgMovie_datas,  movie_datas=movie_datas)
 
     else:
-        print("request method is post")
+        user_id = session.get("user_id")
+        if user_id: 
+            print("request method is post")
 
-        favorite_action = request.form.get('favorite')
-        watchlist_action = request.form.get('watchlist')
+            favorite_action = request.form.get('favorite')
+            watchlist_action = request.form.get('watchlist')
 
-        if favorite_action == "add to favorites":
-            add_favorite(username, id, "movie", movie_datas["title"])
-        elif favorite_action == "remove from favorites":
-            remove_favorite(username, id)
+            if favorite_action == "add to favorites":
+                add_favorite(username, id, "movie", movie_datas["title"])
+            elif favorite_action == "remove from favorites":
+                remove_favorite(username, id)
 
-        if watchlist_action == "add to watchlist":
-            add_to_watchlist(username, id, "movie", movie_datas["title"])
-        elif watchlist_action == "remove from watchlist":
-            remove_from_watchlist(username, id)
+            if watchlist_action == "add to watchlist":
+                add_to_watchlist(username, id, "movie", movie_datas["title"])
+            elif watchlist_action == "remove from watchlist":
+                remove_from_watchlist(username, id)
 
 
-        return redirect(url_for("movies.movie", id=id))
+            return redirect(url_for("movies.movie", id=id))
+        
