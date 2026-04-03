6082455a2acaef87c18138993adfecf96b251181
demandsupply <giova.zol92@gmail.com>
2026-04-03 13:56:17 +0200

    fix: updated connection to NEON db

--- init_db.py
+++ init_db.py
@@ -4,10 +4,12 @@ from cs50 import SQL
 
 load_dotenv()
 
-db = SQL(
-    f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-)
+# db = SQL(
+#     f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+#     f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+# )
+
+db = SQL(os.getenv("DATABASE_URL"))
 
 tables = ["compareshows", "usershows", "favoriteswatchlist", "users"]
 for table in tables:
